5b98651e529a7f151197679c9109e4b88fd318bb
John Morris <2e0byo@gmail.com>
2025-11-01 15:40:46 +0100

    feat: httpx client

---
+++ talk1/httpx_client.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python3
+import httpx
+
+PORT = 8000
+SRV = "localhost"
+
+def fetch() -> str:
+    return httpx.get(f"http://{SRV}:{PORT}").text
+
+if __name__ == "__main__":
+    print(fetch())
