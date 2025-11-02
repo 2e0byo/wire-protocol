3a5f5ebe190bcf1e4003bea366747e48f2ce4cfb
John Morris <2e0byo@gmail.com>
2025-11-02 13:18:25 +0100

    chore: format

--- talk1/httpx_client.py
+++ talk1/httpx_client.py
@@ -1,11 +1,13 @@
-#/usr/bin/env python3
+#! /usr/bin/env python3
 import httpx
 
 PORT = 8000
 SRV = "localhost"
 
+
 def fetch() -> str:
     return httpx.get(f"http://{SRV}:{PORT}").text
+
 
 if __name__ == "__main__":
     print(fetch())
